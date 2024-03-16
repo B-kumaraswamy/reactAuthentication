c84009318d7793983c170aca898d7bf08b53bc6f
B-kumaraswamy <152007390+B-kumaraswamy@users.noreply.github.com>
2024-03-16 15:48:03 +0530

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import Header from "./header";
 import Cookies from 'js-cookie';
-import { Route, Navigate } from "react-router-dom";
+import {  Navigate } from "react-router-dom";
 
 function Home() {
 
